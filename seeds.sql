744e9fd6655ee0c1eb94234b05fb682237feb5a3
Greg Scott <gtscott90@gmail.com>
2021-02-10 00:37:45 -0500

    updated seeds file

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+-- INSERT INTO tableName (column 1, column 2) VALUES (value for column 1, value for column 2)
+
+INSERT INTO department (id, name) VALUES (1, "Engineering");
+INSERT INTO department (id, name) VALUES (2, "Accounting");
+INSERT INTO department (id, name) VALUES (3, "Marketing");
+
+INSERT INTO role (id, title, salary, department_id) VALUES (1, "Lead Enginner", 100000.00, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (2, "Enginner", 80000.00, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (3, "Junior Engineer", 60000.00, 1);
+INSERT INTO role (id, title, salary, department_id) VALUES (4, "Accountant", 95000.00, 2);
+INSERT INTO role (id, title, salary, department_id) VALUES (5, "Fundraising", 55000.00, 2);
+INSERT INTO role (id, title, salary, department_id) VALUES (6, "Online Marketing", 75000.00, 3);
+INSERT INTO role (id, title, salary, department_id) VALUES (7, "Other Marketing", 70000.00, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id) VALUES (1, Jess, Jones, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, Mark, Williams, 2, 1);
